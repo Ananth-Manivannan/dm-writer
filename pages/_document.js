568ab15acbca962588b8a1b6ff3f37734069b122
Ananth-Manivannan <ananth.mani27@gmail.com>
2023-01-23 16:12:02 -0500

    save change to document file to fix console error.

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <meta property="og:title" content="LinkedIn Genie" key="title"/>
-        <meta property="og:description" content="Generate personalized LinkedIn dms" key="description"/>
+        <meta property="og:description" content="Generate personalized linkedin DMs" key="description"/>
         <meta
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
